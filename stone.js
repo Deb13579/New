59c2061a86d6c98de64a93b72b345166c720f8ec
Deb13579 <74122863+Deb13579@users.noreply.github.com>
2021-01-24 08:39:37 +0800

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,26 @@
+class Stone {
+    constructor(x, y, radius) {
+      var options = {
+        isStatic: false , 
+        restitution:0.8,
+        friction:1,
+        density:1
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius
+      this.image = loadImage("Plucking mangoes/stone.png")
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      pos.x = mouseX
+      pos.y = mouseY
+      push();
+     // translate(pos.x, pos.y);
+      imageMode(CENTER);
+      fill("black");
+      image(this.image,pos.x,pos.y, 50, 50)
+  
+      pop();
+    }
+  };
